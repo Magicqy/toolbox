6f56cad93865b2987716a7c19a3d31143c1cc43c
QuYong <work.qu@outlook.com>
2015-09-18 13:41:48 +0800

    android manifest xml meta-data modifier

---
+++ meta_data_modifier.py
@@ -0,0 +1,38 @@
+﻿#!/usr/bin/python
+import os, sys, argparse, xml.etree.ElementTree
+
+def ModifyMetaData(srcFile, dstFile, newValues):
+    ns = 'http://schemas.android.com/apk/res/android'
+    fname, fvalue = '{%s}name' %ns, '{%s}value' %ns
+
+    xml.etree.ElementTree.register_namespace('android', ns)
+    xmltree = xml.etree.ElementTree.parse(srcFile)
+    appNode = xmltree.find('application')
+    if appNode != None:
+        allmd = appNode.findall('meta-data')
+        for md in allmd:
+            name, value = md.get(fname), md.get(fvalue)
+            if name in newValues:
+                md.set(fvalue, newValues[name])
+                print('name="%s" (value="%s") => (value="%s")' %(name, value, newValues[name]))
+    else:
+        print('application node not found, invalid AndroidManifest.xml')
+    
+    xmltree.write(dstFile, 'utf-8', True)
+    print('modify finished')
+pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'AndroidManifest meta data modifier')
+    parser.add_argument('srcFile', help = 'source xml file')
+    parser.add_argument('dstFile', help = 'new xml file to save')
+    parser.add_argument('-md', nargs = 2, action = 'append', required = True,
+                        help = 'meta-data name/value pair to modify')
+    args = parser.parse_args()
+
+    if args.md and len(args.md) > 0:
+        newValues = {}
+        for item in args.md:
+            newValues[item[0]] = item[1]
+        ModifyMetaData(args.srcFile, args.dstFile, newValues)
+    pass
